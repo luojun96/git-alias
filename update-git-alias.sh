ac315f98c533d8d0d02514c30e4fc408061ae31e
Jun Luo <luojun96@live.cn>
2023-06-11 03:11:39 +0800

    Update update-git-alias.sh

--- update-git-alias.sh
+++ update-git-alias.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-# update git config alias based on the gitconfig-alias file
-
 # get the current directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # get the git config alias json file in current directory 
 GITCONFIG_ALIAS_FILE=$DIR/gitconfig-alias.json
 
-# check the file exists
+# check the git config alias json file exists
 if [ ! -f $GITCONFIG_ALIAS_FILE ]; then
     echo "gitconfig-alias.json file not found"
     exit 1
@@ -17,7 +15,7 @@ fi
 # read the gitconfig alias json file
 GITCONFIG_ALIAS_JSON=$(cat $GITCONFIG_ALIAS_FILE)
 
-# get the length of the json array in the key "alias""
+# get the length of the json array in the key "alias"
 GITCONFIG_ALIAS_JSON_LENGTH=$(echo $GITCONFIG_ALIAS_JSON | jq '.alias | length')
 
 # loop through the json array
